8fb5259f1025a27bc2e3ed8fb0d6e14b6e25e353
Yassa Taiseer <yassataiseer@gmail.com>
2021-10-01 15:40:10 -0400

    read and write is done

--- Services/services_manager.py
+++ Services/services_manager.py
@@ -7,7 +7,7 @@ from decouple import config
 from flask_sqlalchemy import SQLAlchemy
 import sys
 import os.path
-from models import Clients,db
+from models import Services,db
 from flask import Flask, jsonify,Blueprint
 from sqlalchemy import exc
 
@@ -17,11 +17,11 @@ sys.path.append(
 services_template = Blueprint("services_template",__name__)## extends app.py
 
 
-class Services:
+class Service:
     def add_service(Service_name,Service_purpose,Service_cost):
         ## Add a new service to database
         try:
-            query = Services(Service_name=Service_name,Service_purpose=Service_purpose,Service_cost=Service_cost)
+            query = Services(Service_Name=Service_name,Service_Purpose=Service_purpose,Service_Cost=Service_cost)
             db.session.add(query)
             db.session.commit()
             return {"Status":True}
@@ -30,7 +30,7 @@ class Services:
     def delete_service(Service_name):
         ## Deletes service from database 
         try:
-            Services.query.filter_by(Service_name=Service_name).delete()
+            Services.query.filter_by(Service_Name=Service_name).delete()
             db.session.commit()
             return {"Status": True}
         except exc.SQLAlchemyError:
@@ -38,7 +38,7 @@ class Services:
     def update_service(Service_name,Service_purpose,Service_cost):
         ## Updates existing service row
         try:
-            current_service = Services.query.filter_by(Service_name=Service_name).first()
+            current_service = Services.query.filter_by(Service_Name=Service_name).first()
             current_service.Service_name = Service_name
             current_service.Service_purpose = Service_purpose
             current_service.Service_cost = Service_cost
@@ -46,14 +46,14 @@ class Services:
             return {"Status":True}
         except exc.SQLAlchemyError:
             return {"Status":False}
-            
+
     def grab_services():
         ## Grab all data from services
         all_data = Services.query.all()
         final_data = []
         for i in range(len(all_data)):
-            row = {"Service_name":all_data[i].Service_name,"Service_purpose":all_data[i].Service_purpose,
-            "Service_cost":all_data[i].Service_cost}
+            row = {"Service_name":all_data[i].Service_Name,"Service_purpose":all_data[i].Service_Purpose,
+            "Service_cost":float(all_data[i].Service_Cost)}
             final_data.append(row)
         return final_data
         
--- Services/services_template.py
+++ Services/services_template.py
@@ -2,11 +2,27 @@
 
 from flask import Flask, json, jsonify,Blueprint
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
-from Services.services_manager import *
+from Services.services_manager import Service
 from flask_cors import CORS
+
 
 
 services_template = Blueprint("services_template",__name__)## extends app.py
 
 api = Api(services_template)
 CORS(services_template, resources={r"/api/*": {"origins": "*"}})
+
+
+class mk_Service(Resource):
+    def get(self,Service_name,Service_purpose,Service_cost):
+        status = Service.add_service(Service_name,Service_purpose,Service_cost)
+        return jsonify(status)
+
+class all_Services(Resource):
+    def get(self):
+        data = Service.grab_services()
+        return jsonify(data)
+
+
+api.add_resource(mk_Service,"/mk_Service/<string:Service_name>/<string:Service_purpose>/<float:Service_cost>")##creates client
+api.add_resource(all_Services,"/all_Services")##creates client
